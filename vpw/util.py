17bc821e3674b438cf8fe36d0b0d089f4bdc8376
Berin Martini <bmartini@gmail.com>
2022-01-23 14:58:45 -0800

    Move parser from build.py to vpw package
    First step to in making the vpw package an all-in-one testbench framework and
    thus allow for the remove of the Makefile.

--- vpw/util.py
+++ vpw/util.py
@@ -3,9 +3,20 @@ Verilator Python Wrapper Package
 """
 
 from typing import List
+from typing import Dict
+from typing import Tuple
+
+from parsy import seq  # type: ignore
+from parsy import regex  # type: ignore
+from parsy import string  # type: ignore
+from parsy import ParseError  # type: ignore
 
 from math import ceil
-
+import re
+from subprocess import PIPE
+import sys
+import subprocess
+import importlib
 
 # Maintains persistent background tasks in the form of a list of generators
 # that get incremented every clock cycle.
@@ -64,7 +75,6 @@ def tick():
         except StopIteration:
             background.remove(gen)
 
-
     return io
 
 
@@ -77,3 +87,156 @@ def idle(time: int = 1):
 
 def finish():
     dut.finish()
+
+
+def parse(name: str = 'example', clock: str = 'clk'):
+
+    def generate_intro(name: str) -> str:
+        return f'#include "V{name}.h"\n' \
+               f'#include "verilated.h"\n' \
+               f'#include "verilated_vcd_c.h"\n' \
+               f'\n' \
+               f'typedef V{name} TB;\n' \
+               f'#include "testbench.hh"\n' \
+               f'\n' \
+               f'\n' \
+               f'void prep(const std::string port,' \
+               f' const std::vector<uint64_t> &value) {"{"}\n' \
+               f'\n' \
+               f'  '
+
+    def generate_prep_if(port: str, width: str) -> str:
+        if (width != 'IN8' and width != "IN16" and width != "IN32" and
+                width != "IN64" and width != "INW"):
+            return ''
+        else:
+            return f'if ("{port}" == port) {"{"}\n'
+
+    def generate_prep_body(port: str, width: str) -> str:
+        if width == 'IN8':
+            return f'    dut->{port} = static_cast<const uint8_t>(value[0]);\n  {"}"} else '
+        elif width == "IN16":
+            return f'    dut->{port} = static_cast<const uint16_t>(value[0]);\n  {"}"} else '
+        elif width == "IN32":
+            return f'    dut->{port} = static_cast<const uint32_t>(value[0]);\n  {"}"} else '
+        elif width == "IN64":
+            return f'    dut->{port} = static_cast<const uint64_t>(value[0]);\n  {"}"} else '
+        elif width == "INW":
+            return f'    for (std::size_t i = 0; i != value.size(); ++i) {"{"}\n' \
+                   f'       dut->{port}[i] = static_cast<const uint32_t>(value[i]);\n' \
+                   f'    {"}"}\n  {"}"} else '
+        else:
+            return ''
+
+    def generate_update() -> str:
+        return f'{"{"}\n' \
+               f'    printf("WARNING: requested port \\\'%s\\\' not found.\\n", ' \
+               f'port.c_str());\n' \
+               f'  {"}"}\n' \
+               f'{"}"}\n' \
+               f'\n' \
+               f'py::dict update() {"{"}\n\n'
+
+    def generate_update_list(port: str, width: str) -> str:
+        if width != "INW" and width != "OUTW":
+            return ''
+        else:
+            return f'  py::list {port};\n' \
+                   f'  for (auto &item : dut->{port}) {"{"}\n' \
+                   f'    {port}.append(item);\n' \
+                   f'  {"}"}\n\n'
+
+    def generate_update_return() -> str:
+        return "  return py::dict ("
+
+    def generate_update_dict(port: str, width: str) -> str:
+        if width != "INW" and width != "OUTW":
+            return f'    "{port}"_a = dut->{port}'
+        else:
+            return f'    "{port}"_a = {port}'
+
+    def generate_update_end() -> str:
+        return "\n  );\n}\n"
+
+    def create_cpp(name: str, ports: Dict[str, str]) -> str:
+        body = ''
+
+        body += generate_intro(name)
+
+        for port, width in ports.items():
+            body += generate_prep_if(port, width)
+            body += generate_prep_body(port, width)
+
+        body += generate_update()
+
+        for port, width in ports.items():
+            body += generate_update_list(port, width)
+
+        body += generate_update_return()
+
+        port = list(ports.keys())[0]
+        body += '\n' + generate_update_dict(port, ports[port])
+        del ports[port]
+
+        for port, width in ports.items():
+            body += ',\n' + generate_update_dict(port, width)
+
+        body += generate_update_end()
+        return body
+
+    def parse_header(name: str, clock: str) -> Tuple[str, Dict[str, str]]:
+        in8 = string('    VL_IN8(').map(lambda x: 'IN8')
+        in16 = string('    VL_IN16(').map(lambda x: 'IN16')
+        in32 = string('    VL_IN(').map(lambda x: 'IN32')
+        in64 = string('    VL_IN64(').map(lambda x: 'IN64')
+        inw = string('    VL_INW(').map(lambda x: 'INW')
+
+        out8 = string('    VL_OUT8(').map(lambda x: 'OUT8')
+        out16 = string('    VL_OUT16(').map(lambda x: 'OUT16')
+        out32 = string('    VL_OUT(').map(lambda x: 'OUT32')
+        out64 = string('    VL_OUT64(').map(lambda x: 'OUT64')
+        outw = string('    VL_OUTW(').map(lambda x: 'OUTW')
+
+        ports = (in8 | in16 | in32 | in64 | inw | out8 | out16
+                 | out32 | out64 | outw).desc('variable width definition')
+
+        varname = regex(r'[a-zA-Z_0-9]+').desc('variable name')
+
+        with open(f'{name}/V{name}.h', 'r') as f:
+            lines = f.readlines()
+
+        portlist = {}
+        for line in lines:
+            # Collapse differences between different verilator versions
+            line = re.sub(r'(\sVL_[A-Z0-9]*)\([\(\&]*([a-zA-Z0-9_]+)\)?,', r'\1(\2,', line)
+            try:
+                port_def, _ = seq(ports, varname).parse_partial(line)
+                portlist[port_def[1]] = port_def[0]
+            except ParseError:
+                pass
+
+        # remove clock from port list
+        if clock in portlist:
+            del portlist[clock]
+        else:
+            print(f"Clock signal '{clock}' not in portlist: ",
+                  portlist.keys(), file=sys.stderr)
+
+        return name, portlist
+
+    # Build
+    output = subprocess.run(['python3-config', '--extension-suffix'], stdout=PIPE, text=True)
+    print(f"{output.stdout.strip()}")
+
+    subprocess.run(['rm', '-rf', f'{name}/', f'{name}*.so', f'{name}.vcd']),
+
+    subprocess.run(['make', f'NAME={name}', f'{name}/V{name}.cpp'])
+
+    code = create_cpp(*parse_header(name, clock))
+
+    with open(f'{name}/{name}.cc', 'w') as f:
+        f.write(code)
+
+    subprocess.run(['make', f'NAME={name}', f'CLOCK={clock}'])
+
+    return importlib.import_module(name)
