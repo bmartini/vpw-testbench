8ceded55945867341d05f64a0339bdc0331dfbf0
Berin Martini <bmartini@gmail.com>
2022-02-24 22:02:19 -0800

    Add setup.py to project to enable package install

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name = "vpw-testbench",
+    version = "1",
+    author = "Berin Martini",
+    author_email = "berin.martini@gmail.com",
+    description = ("Verilator Python Wrapper and testbench framework"),
+    license = "MIT",
+    keywords = "verilator",
+    url = "https://github.com/bmartini/vpw-testbench",
+    packages=["vpw"],
+    package_data = {
+        "vpw": ["testbench.hh"],
+    },
+)
